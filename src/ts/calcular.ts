485ad79bd69973392828581eb7ae59a09886ca2c
vitukjkkk <vittorcolom@gmail.com>
2024-11-23 16:47:35 -0300

    terminando modulo estatística

--- src/ts/calcular.ts
+++ src/ts/calcular.ts
@@ -1,6 +1,9 @@
 import '../css/index.css';
 
+// BIBLIOTECAS
+
 import { control } from './config';
+const ss = require('simple-statistics');
 
 // ELEMENTOS
 
@@ -19,15 +22,29 @@ const imgHeaderIconSobre = document.querySelector('#imgHeaderIconSobre') as HTML
 const calcularSelect = document.querySelector('#calcularSelect') as HTMLSelectElement;
 
 // MODULES BUTTONS
-const basicaButtons = document.querySelectorAll<HTMLElement>('.basicaButtons');
-const mathButtons = document.querySelectorAll<HTMLElement>('.mathButtons');
 const fundamentalButtons = document.querySelectorAll<HTMLElement>('.fundamentalButtons');
 
-const inputCalcularMain = document.querySelector('#inputCalcularMain') as HTMLInputElement;
+const basicaButtons = document.querySelectorAll<HTMLElement>('.basicaButtons');
+const mathButtons = document.querySelectorAll<HTMLElement>('.mathButtons');
+const financebuttons = document.querySelectorAll<HTMLElement>('.financebuttons');
+const staticbuttons = document.querySelectorAll<HTMLElement>('.staticbuttons');
+
 const divMainCalcular = document.querySelectorAll<HTMLElement>('.divMainCalcular');
+
+// INPUTS
+const inputCalcularMain = document.querySelector('#inputCalcularMain') as HTMLInputElement;
 const inputMathSecond = document.querySelector('#inputMathSecond') as HTMLInputElement;
+const inputFinanceTaxa = document.querySelector('#inputFinanceTaxa') as HTMLInputElement;
+const inputFinancePeriodo = document.querySelector('#inputFinancePeriodo') as HTMLInputElement;
 
 // VARIABLES
+
+const inputs = [
+    inputCalcularMain,
+    inputMathSecond,
+    inputFinanceTaxa,
+    inputFinancePeriodo
+]
 
 var pagesHeaderVisible = false;
 
@@ -75,19 +92,78 @@ aHeaderPages.forEach((page, index) => {
 
 inputCalcularMain.min = '0';
 
-inputCalcularMain.oninput = () => {
-    inputCalcularMain.value = inputCalcularMain.value.replace(/[^0-9\+\-\*\/\(\)\.]/g, '');
-}
 
-inputMathSecond.oninput = () => {
-    inputMathSecond.value = inputMathSecond.value.replace(/[^0-9\.]/g, '');
-}
+
+inputs.forEach((input) => {
+    input.oninput = () => {
+        input.value = input.value.replace(/[^0-9\+\-\*\/\(\)\.]/g, '');
+    }
+});
 
 inputCalcularMain.onkeydown = (event) => {
     if(event.key === 'Enter') {
         calcularModulo(0);
     } 
 }
+
+calcularSelect.onchange = () => {
+    
+    divMainCalcular.forEach((div) => {
+        div.style.display = 'none';
+    });
+
+    switch(calcularSelect.value) {
+        case 'basica':
+            divMainCalcular[0].style.display = 'block';
+            control.lastModuleShow = 0;
+            break;
+        case 'matematica':
+            divMainCalcular[1].style.display = 'block';
+            control.lastModuleShow = 1;
+            break;
+        case 'cientifica':
+            divMainCalcular[2].style.display = 'block';
+            control.lastModuleShow = 2;
+            break;
+        case 'financeira':
+            divMainCalcular[3].style.display = 'block';
+            control.lastModuleShow = 3;
+            break;
+        case 'grafica':
+            divMainCalcular[4].style.display = 'block';
+            control.lastModuleShow = 4;
+            break;
+        case 'estatistica':
+            divMainCalcular[5].style.display = 'block';
+            control.lastModuleShow = 5;
+            break;
+        case 'data':
+            divMainCalcular[6].style.display = 'block';
+            control.lastModuleShow = 6;
+            break;
+        case 'engenharia':
+            divMainCalcular[7].style.display = 'block';
+            control.lastModuleShow = 7;
+            break;
+    }
+}
+
+fundamentalButtons.forEach((button, index) => {
+    button.onclick = () => {
+        switch(index) {
+            case 0: // BOTÃO APAGAR
+                inputCalcularMain.value = inputCalcularMain.value.substring(0, inputCalcularMain.value.length - 1);
+                break;
+            case 1: // BOTÃO LIMPAR
+                inputCalcularMain.value = "";
+                inputMathSecond.value = "";
+                break;
+            case 2:
+                calcularModulo(0);
+                break;
+        }
+    }
+});
 
 basicaButtons.forEach((button, index) => {
     button.onclick = () => {
@@ -154,62 +230,64 @@ mathButtons.forEach((button, index) => {
     }
 });
 
-calcularSelect.onchange = () => {
-    
-    divMainCalcular.forEach((div) => {
-        div.style.display = 'none';
-    });
-
-    switch(calcularSelect.value) {
-        case 'basica':
-            divMainCalcular[0].style.display = 'block';
-            control.lastModuleShow = 0;
-            break;
-        case 'matematica':
-            divMainCalcular[1].style.display = 'block';
-            control.lastModuleShow = 1;
-            break;
-        case 'cientifica':
-            divMainCalcular[2].style.display = 'block';
-            control.lastModuleShow = 2;
-            break;
-        case 'financeira':
-            divMainCalcular[3].style.display = 'block';
-            control.lastModuleShow = 3;
-            break;
-        case 'grafica':
-            divMainCalcular[4].style.display = 'block';
-            control.lastModuleShow = 4;
-            break;
-        case 'estatistica':
-            divMainCalcular[5].style.display = 'block';
-            control.lastModuleShow = 5;
-            break;
-        case 'data':
-            divMainCalcular[6].style.display = 'block';
-            control.lastModuleShow = 6;
-            break;
-        case 'engenharia':
-            divMainCalcular[7].style.display = 'block';
-            control.lastModuleShow = 7;
-            break;
-    }
-}
-
-fundamentalButtons.forEach((button, index) => {
+financebuttons.forEach((button, index) => {
     button.onclick = () => {
         switch(index) {
-            case 0: // BOTÃO APAGAR
-                inputCalcularMain.value = inputCalcularMain.value.substring(0, inputCalcularMain.value.length - 1);
+            case 0: // JUROS SIMPLES
+                const js = Number(inputCalcularMain.value) * Number(inputFinanceTaxa.value) * Number(inputFinancePeriodo.value);
+                inputCalcularMain.value = js.toString();
                 break;
-            case 1: // BOTÃO LIMPAR
-                inputCalcularMain.value = "";
-                inputMathSecond.value = "";
+            case 1: // JUROS COMPOSTOS
+                const jc = Number(inputCalcularMain.value) * Math.pow(1 + Number(inputFinanceTaxa.value), Number(inputFinancePeriodo.value));
+                inputCalcularMain.value = jc.toString();
                 break;
-            case 2:
-                calcularModulo(0);
+            case 2: // VALOR PRESENTE
+                const vp = Number(inputCalcularMain.value) / Math.pow(1 + Number(inputFinanceTaxa.value), Number(inputFinancePeriodo.value));
+                inputCalcularMain.value = vp.toString();
+                break;
+            case 3: // VALOR FUTURO
+                const vf = Number(inputCalcularMain.value) * Math.pow(1 + Number(inputFinanceTaxa.value), Number(inputFinancePeriodo.value));
+                inputCalcularMain.value = vf.toString();
+                break;
+            case 4: // AMORTIZAÇÃO
+                const amort = Number(inputCalcularMain.value) / ((1 - Math.pow(1 + Number(inputFinanceTaxa.value), - Number(inputFinancePeriodo.value))) / Number(inputFinanceTaxa.value));
+                inputCalcularMain.value = amort.toString();
                 break;
         }
+        inputCalcularMain.focus();
+    }
+});
+
+staticbuttons.forEach((button, index) => {
+    button.onclick = () => {
+        const numbers = inputCalcularMain.value.split('-').map(Number);
+        switch(index) {
+            case 0: // MÉDIA
+                inputCalcularMain.value = ss.mean(numbers).toString();
+                break;
+            case 1: // MEDIANA
+                inputCalcularMain.value = ss.median(numbers).toString();
+                break;
+            case 2: // MODA
+                inputCalcularMain.value = ss.mode(numbers).toString();
+                break;
+            case 3: // VARIÂNCIA
+                inputCalcularMain.value = ss.variance(numbers).toString();
+                break;
+            case 4: // DESVIO PADRÃO
+                inputCalcularMain.value = ss.standardDeviation(numbers).toString();
+                break;
+            case 5: // COEFICIENTE DE VARIAÇÃO
+                inputCalcularMain.value = ss.coefficientOfVariation(numbers).toString();
+                break;
+            case 6: // MÁXIMO
+                inputCalcularMain.value = ss.max(numbers).toString();
+                break;
+            case 7: // MÍNIMO
+                inputCalcularMain.value = ss.min(numbers).toString();
+                break;
+        }
+        inputCalcularMain.focus();
     }
 });
 
